3b1f89b03a1c0e9530f0688c80c6e8c7aa6fc56c
vasuy <vasuyaragorla7@gmail.com>
2024-12-25 19:59:32 +0530

    introduction/passing arguments

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of the variables
+Person1=$1
+Person2=$2
+
+#referring of the variables
+echo "$Person1:: Hello $Person2, How are you?"
+echo "$Person2:: Hi $Person1, I am fine. How are you?"
+echo "$Person1:: I am fine too. how is your work?"
+echo "$Person2:: not bad. I am thinking to upgrade to devops"
