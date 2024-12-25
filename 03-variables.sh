92af2246f3707aae330d26088b48ff21b3147ea6
vasuy <vasuyaragorla7@gmail.com>
2024-12-25 19:42:17 +0530

    introduction/passing arguments

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=Ramesh #no space between '=/equal' and value
+Person2=Suresh
+
+echo "$Person1:: Hello $Person2, How are you?"
+echo "$Person2:: Hi $Person1, I am fine. How are you?"
+echo "$Person1:: I am fine too. how is your work?"
+echo "$Person2:: not bad. I am thinking to upgrade to devops"
