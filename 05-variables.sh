468d88d87f26ff0b5edf1edbd5d6365128277cb3
vasuy <vasuyaragorla7@gmail.com>
2024-12-25 22:03:52 +0530

    introduction/passing arguments

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please enter username::"
+
+read USERNAME  #here USERNAME is variable
+
+echo "please enter password::"
+
+read PASSWORD
+
+echo "username is: $USERNAME, Password is: $PASSWORD"
+ 
