a1dc6ce49db382f94914a8b6b40df938fb325d5e
vasuy <vasuyaragorla7@gmail.com>
2024-12-25 19:54:38 +0530

    introduction/passing arguments

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+#declaring of the variables
 Person1=gopi #no space between '=/equal' and value
 Person2=vasu
 
+#referring of the variables
 echo "$Person1:: Hello $Person2, How are you?"
 echo "$Person2:: Hi $Person1, I am fine. How are you?"
 echo "$Person1:: I am fine too. how is your work?"
