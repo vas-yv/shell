9f9171540a656fe698ed2794cba156d1b178c282
vasuy <vasuyaragorla7@gmail.com>
2024-12-26 15:18:13 +0530

    special variables

---
+++ 08-special-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "All variables: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
